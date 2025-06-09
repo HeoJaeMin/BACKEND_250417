32a2cb81093339d23ec28cd03c9f6096031ec655
JaeMin <jemin.heo@airpremia.com>
2025-06-09 11:32:02 +0900

    domain

--- src/main/kotlin/example/jemin/demo/modules/place/domain/Place.kt
+++ src/main/kotlin/example/jemin/demo/modules/place/domain/Place.kt
@@ -1,16 +1,11 @@
 package example.jemin.demo.modules.place.domain
 
 data class Place(val id: Long?, val name: String, val address: Address) {
-
     data class Address(val region: Region, val fullName: String, val building: Building) {
-        data class Region(
-            val depthFirst: String,
-            val depthSecond: String,
-            val depthThird: String,
-            val roadName: String,
-            val coordinate: Coordinate,
-        ) {
+        data class Region(val depth: Depth, val roadName: String, val coordinate: Coordinate) {
             data class Coordinate(val xAxis: Double, val yAxis: Double)
+
+            data class Depth(val first: String, val second: String, val third: String)
         }
 
         data class Building(
